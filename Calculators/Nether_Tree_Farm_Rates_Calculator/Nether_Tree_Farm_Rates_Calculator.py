97f8bc3a0d08da63b231ece0391824ca716a38bb
ncolyer11 <emeraldcreeper7@outlook.com>
2023-04-22 23:01:44 +1000

    additional main menu changes

--- Calculators/Nether_Tree_Farm_Rates_Calculator/Nether_Tree_Farm_Rates_Calculator.py
+++ Calculators/Nether_Tree_Farm_Rates_Calculator/Nether_Tree_Farm_Rates_Calculator.py
@@ -92,7 +92,7 @@ def calculate(dispenser_value, dispenser_frequency_value, hat_frequency_value, t
     bm_used = (1 / fungus_growth_chance + (18000 - 11423) / 14608) * fungus
 
     # stems, shroomlights and wart blocks produced per hour
-    if schematic_path_val.get() == 'empty_layout.litematic':
+    if schematic_path_val.get() == '../../assets/empty_layout.litematic':
         stems_per_cycle = 0
         layer = trunk_start_value - 1
         while layer < trunk_height_value:
@@ -134,10 +134,8 @@ def calculate(dispenser_value, dispenser_frequency_value, hat_frequency_value, t
     for label_num in range(0, 11):
         labels[label_num + 16]['text'] = output_value_labels[label_num]
 
-    # print('calculated :D')
 
-
-schematic_path_val = tk.StringVar(value='empty_layout.litematic')
+schematic_path_val = tk.StringVar(value='../../assets/empty_layout.litematic')
 
 
 def open_file_explorer():
--- Calculators/Stemlight_Calculator/Main_Menu.py
+++ Calculators/Stemlight_Calculator/Main_Menu.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import tkinter.font as font
+import threading
 import os
 
 
@@ -7,9 +9,35 @@ def open_file(folder, file_name):
     os.system(f"python ../{folder}/{file_name}")
 
 
+def open_file_single_instance(folder, file):
+    thread = threading.Thread(target=open_file, args=(folder, file))
+    thread.start()
+
+
+bg_colour = '#191919'
+fg_colour = '#FFFFFF'
+bg_widget_colour = '#323231'
+fg_button_colour = '#323231'
+subheading_colour = '#FA873A'
+
 # Create main window
 root = tk.Tk()
-root.title("Main Menu")
+root.title("Stemlight: Main Menu")
+root.iconbitmap('../../assets/ikon.ico')
+root.configure(bg=bg_colour)
+
+# Create menu
+toolbar = tk.Menu(root)
+root.config(menu=toolbar)
+
+file_menu = tk.Menu(toolbar, tearoff=0)
+toolbar.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="Exit", command=root.quit)
+
+# fonts
+main_font = font.Font(family='Segoe UI Semibold', size=11)
+button_font = font.Font(family='Segoe UI Semibold', size=11)
+subheading_font = font.Font(family='Segoe UI Semibold', size=12)
 
 # Define file names and button labels
 file_names = [
@@ -18,19 +46,28 @@ file_names = [
     "Nylium_Dispenser_Placement_Calculator.py",
     "Trunk_Distribution_Calculator.py"
 ]
+
 folder_names = [
     "Nether_Tree_Farm_Layout_Efficiency_Calculator",
     "Nether_Tree_Farm_Rates_Calculator",
     "Nylium_Dispenser_Placement_Calculator",
     "Trunk_Distribution_Calculator"
 ]
-button_labels = ["Layout Efficiency", "Farm Rates", "Nylium Dispenser Placement", "Trunk Distribution"]
+
+button_labels = [
+    "Layout Efficiency",
+    "Farm Rates",
+    "Nylium Dispenser Placement",
+    "Trunk Distribution"
+]
 
 # Create buttons using a for loop
 for i in range(len(file_names)):
-    button = tk.Button(root, text=button_labels[i], command=lambda file=file_names[i], folder = folder_names[i]:
-        open_file(folder, file))
-    button.pack()
+    button = tk.Button(root, text=button_labels[i],
+                       command=lambda file=file_names[i], folder=folder_names[i]: open_file_single_instance(folder,
+                                                                                                            file))
+    button.config(bg=bg_colour, fg=fg_colour, font=main_font, padx=10, pady=10, width=25, height=2)
+    button.grid(row=i // 2, column=i % 2, padx=5, pady=5)
 
 # Start main loop
 root.mainloop()
