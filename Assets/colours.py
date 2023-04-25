8ed6b90fc2689b0a15af8d819273ed61ba48b5ef
ncolyer11 <emeraldcreeper7@outlook.com>
2023-04-25 14:06:32 +1000

    archive and style files

---
+++ Assets/colours.py
@@ -0,0 +1,14 @@
+# define the colors
+bg = "#191919"
+fg = "#F0F0F0"
+off_black = "#1c1c1c"
+bg_widget = "#323231"
+fg_button = "#323231"
+# subheading = "#FA873A"
+crimson = "#D42121"
+warped = "#00a7a3"
+menu_gradient = ["#87CEEB", "#1E90FF"]
+dark_blue_grey = "#1F3B4D"
+dark_orange = "#DB4914"
+light_red = "#D22B2B"
+vermillion = "#DD3224"
