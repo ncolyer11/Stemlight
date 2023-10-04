acf23ebaa4e952b07f5dfe90134c2e88de0b525c
ncolyer11 <emeraldcreeper7@outlook.com>
2023-10-05 01:58:45 +1100

    fixed nylium dispenser placement

--- Calculators/Nylium_Dispenser_Placement_Calculator/Nylium_Dispenser_Placement_Calculator.py
+++ Calculators/Nylium_Dispenser_Placement_Calculator/Nylium_Dispenser_Placement_Calculator.py
@@ -6,6 +6,8 @@
 # sidebar with a list of all dispensers and another adjacent checkbox to select whether or not the block above the
 # dispenser is cleared every cycle
 
+# USE SCHEMATIC OF NYLIUM + DISPENSER INPUT AND GUI LIKE LAYOUT RATES CALCULATOR
+
 width = int(input("Enter Width of Nylium Grid: "))
 length = int(input("Enter Length of Nylium Grid: "))
 dispensers = int(input("Enter Amount of Dispensers: "))
@@ -26,10 +28,12 @@ nyliumGrid = [[0 for i in range(length)] for j in range(width)]
 def selection_chance(x1, y1):
     x2 = 3 - abs(x1)
     y2 = 3 - abs(y1)
-    P = x2 * y2
-    P = 0 if x2 < 0 or y2 < 0 else P
-    return 1 - (1 - P / 81) ** 9
-    # return P
+    P_block = x2 * y2 / 81
+    P_block = 0 if x2 < 0 or y2 < 0 else P_block
+    P = 0
+    for k in range(1, 10):
+        P += (1 - P_block) ** (9 - k)
+    return P * P_block * 11 / 99  # 11/99 for crimson and 13/100 for warped
 
 bonemeal_used = 0
 for i in range(dispensers):
@@ -51,8 +55,9 @@ for x in range(width):
     for y in range(length):
         total += nyliumGrid[x][y]
 
+
 print(f'total items: {total}')
-print(f'crimson fungi: {total / 9}')
+print(f'crimson fungi: {total}')
 print(f'bonemeal_used: {bonemeal_used}')
 
 if dispensers > 0:
