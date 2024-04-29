9b0036b80e7c91f1cda6479968c567ba0bb8daee
ncolyer11 <emeraldcreeper7@outlook.com>
2024-04-29 14:42:52 +1000

    can't figure out the recursive algorithm atm for more than 4 dispensers

--- src/dispenser_placement_optimiser.py
+++ src/dispenser_placement_optimiser.py
@@ -2,6 +2,8 @@ import math
 import numpy as np
 import itertools as iter
 import time
+
+from Assets import constants as const
 from src.Fungus_Distribution_Backend import calculate_fungus_distribution
 
 WARPED = 0
@@ -35,7 +37,7 @@ def brute_force_max_fungi(length, width, num_dispensers, disp_perms, fungus_type
 
     return max_fungi, optimal_rates_coords, all_optimal_coords
 
-def calculate_max_fungi(length, width, num_dispensers, disp_perms, f_type):
+def calculate_max_fungi(length, width, num_dispensers):
     """
     For a 5x5 grid:
     - Start with optimising for 1 dispenser
@@ -50,7 +52,53 @@ def calculate_max_fungi(length, width, num_dispensers, disp_perms, f_type):
     - This process continues until the previous and current optimised positions are the same
     - Then the algorithm should move on to the next dispenser and repeat the process 
     """
-    # - 
+    # Handle 0 dispensers
+    if num_dispensers == 0:
+        return 0, []
+    
+    disp_positions = []
+
+    # Start optimisation
+    for i in range(num_dispensers):
+        disp_positions.append([0,0])
+        optimise_dispenser(i, disp_positions, length, width, num_dispensers)
+
+    return 1, disp_positions
+
+def optimise_dispenser(disp_index, disp_positions, length, width, num_dispensers):
+    max_foliage = -1
+    max_pos = None
+    # Store the current positions to check for changes later
+    prev_positions = disp_positions.copy()
+    # print("hihihi", disp_positions.copy(), disp_index)
+    # Try placing the dispenser at every position in the grid
+    for i, j in iter.product(range(width), range(length)):
+        if [i, j] in disp_positions:
+            continue
+        # Place the dispenser
+        disp_positions[disp_index] = [i, j]
+        # Calculate the total foliage
+        total_foliage, *_ = calculate_fungus_distribution(width, length, len(disp_positions),
+                                                            disp_positions, CRIMSON)
+        # If this position results in more foliage, update the max
+        if total_foliage > max_foliage:
+            max_foliage = total_foliage
+            max_pos = [i, j]
+    
+    # Place the dispenser at the position that resulted in the max foliage
+    disp_positions[disp_index] = max_pos
+    
+    # If the positions haven't changed, we're done
+    while prev_positions != disp_positions:
+        print(f"{disp_index}. Before going down current:\n - {disp_positions}\n - {prev_positions}\n")
+        prev_positions = disp_positions.copy()
+        # Go back and optimise all the previous dispensers
+        if disp_index > 0:
+            optimise_dispenser(disp_index - 1, disp_positions, length, width, num_dispensers)
+        # Go forwards and optimise all the next dispensers
+        if disp_index < len(disp_positions) - 1:
+            optimise_dispenser(disp_index + 1, disp_positions, length, width, num_dispensers)
+        print(f"{disp_index}. After coming back up:\n - {disp_positions}\n - {prev_positions}\n\n")
 
 def output_data(start_time, f_type, width, length, max_rates, max_rates_coords, all_max_coords):
     print(f'Calculated max fungi in {time.time() - start_time:.3f} seconds')
@@ -85,7 +133,7 @@ def output_data(start_time, f_type, width, length, max_rates, max_rates_coords, 
 def initialise_optimisation():
     length = 5
     width = 5
-    num_dispensers = 25
+    num_dispensers = int(input("Enter number of dispensers: "))
     f_type = WARPED
     f_type = CRIMSON
 
@@ -96,21 +144,21 @@ def initialise_optimisation():
     start_time = time.time()
     disp_perms = generate_permutations(length, width, num_dispensers)
     # If the number of permutations is less than 1 million, use brute force
-    if permutations < 1e6:
+    if permutations == 0:
         max_rates, max_rates_coords, all_max_coords = \
             brute_force_max_fungi(length, width, num_dispensers, disp_perms, f_type)
         output_data(start_time, f_type, width, length, max_rates, max_rates_coords, all_max_coords)
     # Otherwise, use the more efficient, but possibly less accurate algorithm
     else:
-        max_rates, max_rates_coords, all_max_coords = \
-            calculate_max_fungi(length, width, num_dispensers, disp_perms, f_type)
-        output_data(start_time, f_type, width, length, max_rates, max_rates_coords, all_max_coords)
+        max_rates, max_rates_coords = \
+            calculate_max_fungi(length, width, num_dispensers)
+        output_data(start_time, f_type, width, length, max_rates, max_rates_coords, max_rates_coords)
 
 initialise_optimisation()
 
-
-# after calculating the most optimal positions to put the dispensers
-# it should compare the difference between the worst possible ordering of those dispensers and the
+# after calculating the most optimal positions to put the dispensers for crimson
+# it should generate a list of all permutations of the order, 4 rotations, and 2 mirrors and then calculate which set of new orientations is best out of those for warped
+# further, out of the entire set, it should compare the difference between the worst possible ordering of those dispensers and the
 # best to see if it's worth worrying about activation order
 
 # need to conventionally define max bm efficiency first get a useful output
