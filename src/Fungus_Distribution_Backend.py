0386b55ce506a24cd275344a5ea432608c46d98e
ncolyer <emeraldcreeper7@outlook.com>
2024-05-18 22:22:06 -0400

    added a dozen (12 (2 * 6)) new (old but just unadded) charts to src/Images

--- src/Fungus_Distribution_Backend.py
+++ src/Fungus_Distribution_Backend.py
@@ -201,7 +201,7 @@ def output_viable_coords(optimal_coords, optimal_value, length, width, wb_per_fu
             bm_for_prod = dist_data["bm_for_prod"]
 
             bm_req = bm_for_prod < wb_per_fungi / const.WARTS_PER_BM - const.AVG_BM_TO_GROW_FUNG
-            if total_des_fungi < optimal_value:
+            if total_des_fungi < optimal_value and bm_req:
                 worst_value = total_des_fungi
             if abs(total_des_fungi - optimal_value) / optimal_value <= 0.001 and bm_req:
                 coords_list_metrics.append((total_des_fungi, bm_for_prod, coords))
