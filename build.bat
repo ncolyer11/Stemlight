90e677bcc94159e7e2fde667df73fb6b5cca5df7
ncolyer <emeraldcreeper7@outlook.com>
2024-11-06 19:21:39 +1100

    added proper pyproject header with req packages

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
 @echo off
 
+:: Activate the Anaconda environment
+CALL conda activate Stemlight
+
+:: Build with PyInstaller
 pyinstaller ^
 --add-data "src/images/*;src/images" ^
 --add-data "src/assets/*;src/assets" ^
@@ -8,4 +12,8 @@ pyinstaller ^
 --windowed ^
 Main_Menu.py
 
+:: Compress the output folder
 powershell Compress-Archive -Path 'dist\\Stemlight for Windows\\*' -Force -DestinationPath 'Stemlight for Windows.zip'
+
+:: Deactivate the environment
+CALL conda deactivate
